d3e95d9bcd6d7e6cddb4118adc098999b2d7e588
suyogparwadi <38881565+suyogparwadi@users.noreply.github.com>
2018-06-12 14:19:59 +0100

    removed account

---
+++ code/crm/common/Defra.CustMaster.D365.Common/ints/idm/resp/AccountResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Defra.CustMaster.D365.Common.Ints.Idm.Resp
+{
+    public class AccountResponse: ResponseCustomerMasterBase
+    {
+
+
+    }
+}
