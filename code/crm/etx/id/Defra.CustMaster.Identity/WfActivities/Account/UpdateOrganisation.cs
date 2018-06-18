ee9c7c8ccd2cc0be5bb09af53ca0c9d9a0c18589
Admin <Admin@DESKTOP-U9SR0TK>
2018-06-18 15:32:23 +0100

    changes

--- code/crm/etx/id/Defra.CustMaster.Identity/WfActivities/Account/UpdateOrganisation.cs
+++ code/crm/etx/id/Defra.CustMaster.Identity/WfActivities/Account/UpdateOrganisation.cs
@@ -18,7 +18,7 @@ using SCIIR = Defra.CustMaster.D365.Common.Ints.Idm.Resp;
 namespace Defra.CustMaster.Identity.WfActivities
 {
     /// <summary>
-    /// {'organisationid': 'D1B35E7C-D072-E811-A83B-000D3AB4F7AF','updates':{'name':'Update my create', 'type':'910400000', 'crn':'18062018','email':'Updateacme@acme.com', 'telephone':'004412345678', 'hierarchylevel':'910400000' , 'parentorganisation':{'parentorganisationcrmid':'89EF9173-016F-E811-A83A-000D3AB4F534'}}}
+    /// {'organisationid': 'D1B35E7C-D072-E811-A83B-000D3AB4F7AF','updates':{'name':'Update my create', 'type':'910400000', 'crn':'18062018','email':'Updateacme@acme.com', 'telephone':'004412345678', 'hierarchylevel':'910400000' , 'parentorganisationcrmid':'89EF9173-016F-E811-A83A-000D3AB4F534'}}
     /// </summary>
     public class UpdateOrganisation : WorkFlowActivityBase
     {
