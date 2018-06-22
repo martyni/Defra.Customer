6cde07393f3c416a88ff40ebd5fa6dd687e5cc42
suyogparwadi <38881565+suyogparwadi@users.noreply.github.com>
2018-06-22 10:52:57 +0100

    connect contact

---
+++ code/crm/etx/id/Defra.CustMaster.Identity/WfActivities/NewFolder1/ConnectContact.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Workflow;
+using Newtonsoft.Json;
+using System;
+using System.Activities;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using SCII = Defra.CustMaster.D365.Common.Ints.Idm;
+using SCIIR = Defra.CustMaster.D365.Common.Ints.Idm.Resp;
+using SCSE = Defra.CustMaster.D365.Common.Schema.ExtEnums;
+using SCS = Defra.CustMaster.D365.Common.schema;
+
+namespace Defra.CustMaster.Identity.WfActivities.Connection
+{
+    public class ConnectContact : WorkFlowActivityBase
+    {
+        #region "Parameter Definition"
+        [RequiredArgument]
+        [Input("PayLoad")]
+        public InArgument<String> PayLoad { get; set; }
+        [Output("OutPutJson")]
+        public OutArgument<string> ReturnMessageDetails { get; set; }
+
+        #endregion
+
+
+        public override void ExecuteCRMWorkFlowActivity(CodeActivityContext executionContext, LocalWorkflowContext crmWorkflowContext)
+        {
+            String PayloadDetails = PayLoad.Get(executionContext);
+            int ErrorCode = 400; //400 -- bad request
+            String _ErrorMessage = string.Empty;
+            String _ErrorMessageDetail = string.Empty;
+            Guid ContactId = Guid.Empty;
+            Guid AccountId = Guid.Empty;
+            StringBuilder ErrorMessage = new StringBuilder();
+            String UniqueReference = string.Empty;
+
+            try
+            {
+
+            }
+
+            #region Catch Exception
+            catch (Exception ex)
+            {
+                crmWorkflowContext.Trace("inside catch");
+                ErrorCode = 500;
+                _ErrorMessage = "Error occured while processing request";
+                _ErrorMessageDetail = ex.Message;
+                ErrorCode = 400;
+                this.ReturnMessageDetails.Set(executionContext, _ErrorMessageDetail);
+
+            }
+            #endregion
+
+            #region Finally Block
+            finally
+            {
+
+            } 
+            #endregion
+        }
+    }
+
+}
