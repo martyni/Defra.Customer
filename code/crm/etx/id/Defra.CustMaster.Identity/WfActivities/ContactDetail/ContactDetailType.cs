dfd33e06bfd58c9c4c96e9e3164a96c0c7b334af
quantumbr <bilal@innostark.com>
2018-07-16 12:14:21 +0100

    Added the Contact Detail for the Add Email

---
+++ code/crm/etx/id/Defra.CustMaster.Identity/WfActivities/ContactDetail/ContactDetailType.cs
@@ -0,0 +1,26 @@
+﻿namespace Defra.CustMaster.Identity.WfActivities
+{
+    using System.Activities;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Workflow;
+
+    public class ContactDetailType : WorkFlowActivityBase
+    {
+        [RequiredArgument]
+        [Input("RequestTypeValue")]
+        public InArgument<int> RequestTypeValue { get; set; }
+
+        [Output("TypeValue")]
+        [AttributeTarget("defra_addressdetails", "defra_addresstype")]
+        public OutArgument<OptionSetValue> TypeValue { get; set; }
+
+        public override void ExecuteCRMWorkFlowActivity(CodeActivityContext executionContext, LocalWorkflowContext crmWorkflowContext)
+        {
+            crmWorkflowContext.Trace("Started: Defra.CustMaster.Identity.WfActivities.ContactDetailType");
+
+            TypeValue.Set(executionContext, new OptionSetValue(RequestTypeValue.Get(executionContext)));
+
+            crmWorkflowContext.Trace("Finished: Defra.CustMaster.Identity.WfActivities.ContactDetailType");
+        }
+    }
+}
