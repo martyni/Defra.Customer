8c5d3768c36a9c1f0f440ca1a7521fd6c67ccf19
quantumbr <bilal@innostark.com>
2018-07-21 00:55:55 +0100

    Corrected the Search activity and created a new Enrolment Type activity

---
+++ code/crm/etx/id/Defra.CustMaster.Identity/WfActivities/LOBServiceUserLink/EnrolmentStatusType.cs
@@ -0,0 +1,26 @@
+﻿namespace Defra.CustMaster.Identity.WfActivities
+{
+    using System.Activities;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Workflow;
+
+    public class EnrolmentStatusType : WorkFlowActivityBase
+    {
+        [RequiredArgument]
+        [Input("OptionValue")]
+        public InArgument<int> OptionValue { get; set; }
+
+        [Output("TypeValue")]
+        [AttributeTarget("defra_lobserviceuserlink", "defra_enrolmentstatus")]
+        public OutArgument<OptionSetValue> TypeValue { get; set; }
+
+        public override void ExecuteCRMWorkFlowActivity(CodeActivityContext executionContext, LocalWorkflowContext crmWorkflowContext)
+        {
+            crmWorkflowContext.Trace("Started: Defra.CustMaster.Identity.WfActivities.EnrolmentStatusType");
+
+            TypeValue.Set(executionContext, new OptionSetValue(OptionValue.Get(executionContext)));
+
+            crmWorkflowContext.Trace("Finished: Defra.CustMaster.Identity.WfActivities.EnrolmentStatusType");
+        }
+    }
+}
