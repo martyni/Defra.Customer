65c51958ac2159c6c0f0cec076d4354a770ddd23
suyogparwadi <38881565+suyogparwadi@users.noreply.github.com>
2018-07-20 15:07:39 +0100

    Added sarch record

---
+++ code/crm/etx/id/Defra.CustMaster.Identity/WfActivities/Common/SearchRecords.cs
@@ -0,0 +1,41 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace Defra.CustMaster.Identity.WfActivities.Common
+{
+   public class SearchRecords : WorkFlowActivityBase
+    {
+        [RequiredArgument]
+        [Input("AdvancedFindXml")]
+        public InArgument<String> fetchQuery { get; set; }
+
+        [RequiredArgument]
+        [Input("ReplaceString")]
+        public InArgument<string> StringToReplace { get; set; }
+
+        [Output("RecordCount")]
+        public OutArgument<Int32> RecordCount { get; set; }
+
+        
+
+        string AdvancedFindXml;
+        string Replacestring;
+        public override void ExecuteCRMWorkFlowActivity(CodeActivityContext executionContext, LocalWorkflowContext crmWorkflowContext)
+        {
+            AdvancedFindXml = fetchQuery.Get(executionContext);
+            crmWorkflowContext.TracingService.Trace("Started: Defra.CustMaster.Identity.WfActivities.ExecuteCRMWorkFlowActivity");
+            //Create the IWorkflowContext and the
+
+            Replacestring = StringToReplace.Get(executionContext);
+            AdvancedFindXml = AdvancedFindXml.Replace("XXX", StringToReplace.Get(executionContext));
+            var results = crmWorkflowContext.OrganizationService.RetrieveMultiple(new FetchExpression(AdvancedFindXml));
+            crmWorkflowContext.Trace(AdvancedFindXml);
+
+            RecordCount.Set(executionContext, results.Entities.Count);
+            crmWorkflowContext.Trace("Finished: Defra.CustMaster.Identity.WfActivities.ExecuteCRMWorkFlowActivity");
+        }
+    }
+}
