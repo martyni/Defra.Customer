27cb68c3f77bac5f40136478b26669034f788646
suyogparwadi <38881565+suyogparwadi@users.noreply.github.com>
2018-07-30 17:27:41 +0100

    added option set value conversion plugin

---
+++ code/crm/etx/id/Defra.CustMaster.Identity/WfActivities/ContactDetail/GetContactDetailsTypeOptionSetValue.cs
@@ -0,0 +1,29 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace Defra.CustMaster.Identity.WfActivities
+{
+   public class GetContactDetailsTypeOptionSetValue : WorkFlowActivityBase
+    {
+
+
+        [RequiredArgument]
+        [Input("Type")]
+        [AttributeTarget("defra_addressdetails", "defra_addresstype")]
+        public InArgument<OptionSetValue> TypeValue { get; set; }
+
+      
+        [Output("RequestTypeValue")]
+        public OutArgument<int> RequestTypeValue { get; set; }
+
+        public override void ExecuteCRMWorkFlowActivity(CodeActivityContext executionContext, LocalWorkflowContext crmWorkflowContext)
+        {
+            crmWorkflowContext.Trace("Started: Defra.CustMaster.Identity.WfActivities.ContactDetailType");
+
+            RequestTypeValue.Set(executionContext, TypeValue.Get(executionContext).Value);
+
+            crmWorkflowContext.Trace("Finished: Defra.CustMaster.Identity.WfActivities.ContactDetailType");
+        }
+    }
+}
