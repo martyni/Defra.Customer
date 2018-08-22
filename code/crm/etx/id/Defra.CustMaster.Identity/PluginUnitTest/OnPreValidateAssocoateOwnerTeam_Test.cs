48ccac2e30e1f2bea0994097788c61398948fa21
suyogparwadi <38881565+suyogparwadi@users.noreply.github.com>
2018-08-22 16:13:07 +0100

    added method to copy team member to owner team

---
+++ code/crm/etx/id/Defra.CustMaster.Identity/PluginUnitTest/OnPreValidateAssocoateOwnerTeam_Test.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Defra.CustMaster.Identity.WfActivities.Connection;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+
+using Defra.CustMaster.Identity.Plugins;
+using System.Collections.Generic;
+using FakeXrmEasy;
+
+
+namespace Defra.PluginUnitTest
+{
+    [TestClass]
+    public class OnPreValidateAssocoateOwnerTeam_Test
+    {
+        [TestMethod]
+        public void OnPreValidateAssocoateOwnerTeamCheckTeamExists_Success()
+        {
+            //Ok, this is going to be our test method body
+
+            //But before doing anything...
+
+            //  FakeXrmEasy is based on the state-based testing paradigm, 
+            //  which is made of, roughly, 3 easy steps:
+
+            //1) We define the initial state of our test.
+
+            //2) Then, we execute the piece of logic which we want to test, 
+            //   which will produce a new state, the final state.
+
+            //3) Finally, we verify that the final state is the expected state (assertions).
+
+            //Let's implement those now
+
+            // 1) Define the initial state
+            // -----------------------------------------------------------------------
+
+            //  Our initial state is going to be stored in what we call a faked context:
+
+            var context = new XrmFakedContext();
+
+            //You can think of a context like an Organisation database which stores entities In Memory.
+
+            //We can also use TypedEntities but we need to tell the context where to look for them, 
+            //this could be done, easily, like this:
+
+            context.ProxyTypesAssembly = Assembly.GetAssembly(typeof(Account));
+
+            //We have to define our initial state now, 
+            //by calling the Initialize method, which expects a list of entities.
+
+            var account = new Account() { Id = Guid.NewGuid(), Name = "My First Faked Account yeah!" };
+
+            context.Initialize(new List<Entity>() {
+                account
+            });
+
+            //With the above example, we initialized our context with a single account record
+
+            // 2) Execute our logic
+            // -----------------------------------------------------------------------
+            //
+            // We need to get a faked organization service first, by calling this method:
+
+            var service = context.GetOrganizationService();
+
+            // That line is the most powerful functionality of FakeXrmEasy
+            // That method has returned a reference to an OrganizationService 
+            // which you could pass to your plugins, codeactivities, etc, 
+            // and, from now on, every create, update, delete, even queries, etc
+            // will be reflected in our In Memory context
+
+            // In a nutshell, everything is already mocked for you... cool, isn't it?
+
+            // Now... 
+
+            // To illustrate this...
+
+            // Let's say we have a super simple piece of logic which updates an account's name
+
+            // Let's do it!
+
+            var accountToUpdate = new Account() { Id = account.Id };
+            accountToUpdate.Name = "A new faked name!";
+
+            service.Update(accountToUpdate);
+
+            // Done!
+
+            //We have successfully executed the code we want to test..
+
+            // Now...
+
+            // The final step is...
+
+            // 3) Verify final state is the expected state
+            // -----------------------------------------------------------------------
+            //
+
+            //We are going to use Xunit assertions.
+
+            var updatedAccountName = context.CreateQuery<Account>()
+                                    .Where(e => e.Id == account.Id)
+                                    .Select(a => a.Name)
+                                    .FirstOrDefault();
+
+
+            //And finally, validate the account has the expected name
+            Assert.Equals("A new faked name!", updatedAccountName);
+
+            // And we are DONE!
+
+            // We have successfully implemented our first test!
+        }
+    }
+}
+    }
+}
+
+        }
+    }
+}
---
+++ code/crm/etx/id/Defra.CustMaster.Identity/Plugins/Security/OnPreValidateAssocoateOwnerTeam.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace Defra.CustMaster.Identity.Plugins.Security
+{
+    public class OnPreValidateAssocoateOwnerTeam : IPlugin 
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            tracingService.Trace("OnPreValidateAssocoateOwnerTeam before assingning teams.");
+
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+                if (entity.LogicalName != "team")
+                    return;
+
+                EntityReferenceCollection RelatedRecords = (EntityReferenceCollection)context.InputParameters["RelatedEntitities"];
+                OrganizationServiceContext orgSvcContext = new OrganizationServiceContext(service);
+
+                String MemberShipTeamName = (String)entity.Attributes["name"];
+                String OwnerTeamName;
+                Guid[] UserIds;
+                int index = MemberShipTeamName.LastIndexOf("-");
+                if (index > 0)
+                {
+                    OwnerTeamName = MemberShipTeamName.Substring(0, index) + "-OWNER";
+                    if (OwnerTeamName != String.Empty)
+                    {
+                        var GetOwnerTeam = from c in orgSvcContext.CreateQuery("team")
+                                           where (string)c["teamid"] == OwnerTeamName 
+                                           && (int)c["statecode"] == 0
+                                           select new
+                                           {
+                                               teamid = c.Id
+                                           };
+
+                        if(GetOwnerTeam.FirstOrDefault() != null)
+                        {
+                            tracingService.Trace("team found");
+
+                            Guid TeamId = GetOwnerTeam.FirstOrDefault().teamid;
+                            //get user 
+                            AddMembersTeamRequest addRequest = new AddMembersTeamRequest();
+                            addRequest.TeamId = TeamId;
+                            UserIds = RelatedRecords.Select(x => x.Id).ToArray();
+                            addRequest.MemberIds = UserIds;
+                            tracingService.Trace("before updating ");
+
+                            service.Execute(addRequest);
+                            tracingService.Trace("after updating ");
+
+                        }
+                    }
+                }
+                tracingService.Trace("PreCreateAccountSetMultiSelectOptionSet after assinging values.");
+            }
+        }
+
+
+    }
+}
